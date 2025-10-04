5810a0833f8f2711eee912df4e8fdf7370d1f2ae
mallikarjunimmadi <64016071+mallikarjunimmadi@users.noreply.github.com>
2025-10-04 13:32:36 +0530

    Update externalHM.py

--- externalHM.py
+++ externalHM.py
@@ -1,4 +1,27 @@
-#!/usr/bin/env python3
+Conversation opened. 1 unread message.
+
+Skip to content
+Using Broadcom Inc. Mail with screen readers
+
+1 of 30,062
+AVI External HM
+External
+Inbox
+
+
+Mallikarjun Immadi
+Attachments
+1:30 PM (1 minute ago)
+to me
+
+
+
+The information in this mail is confidential and is intended solely for addressee. Access to this mail by anyone else is unauthorized. Copying or further distribution beyond the original recipient may be unlawful. Any opinion expressed in this mail is that of sender and does not necessarily reflect that of State Bank group.
+---
+ One attachment
+  •  Scanned by Gmail
+from:immadimallikarjun_vmware@sbi.co.in
+#!/usr/bin/python3
 """
 External Health Monitor for VMware NSX Advanced Load Balancer (Avi)
 Uses Paramiko to SSH into backend server and run 'ipcs -qa | grep 1d6c91 | wc -l'
@@ -16,10 +39,12 @@ server_ip = os.environ.get('IP')      # Backend server IP
 server_port = int(os.environ.get('PORT', 22))  # Default SSH port 22
 
 # Hard-coded credentials:
-username = "myuser"       # change this
-password = "mypassword"   # change this
+username = "toolsuser"       # change this
+password = "toolsuser"   # change this
 
-remote_command = "ipcs -qa | grep 1d6c91 | wc -l"
+#remote_command = "ipcs -qa | grep 1d6c91 | wc -l"
+#remote_command = "hostname"
+remote_command = "ps -ef | grep grafana | grep -v grep | wc -l"
 
 try:
     # Create SSH client
@@ -36,12 +61,17 @@ try:
     ssh.close()
 
     if error:
+        print(f"ext_hm_usr_err_msg: External HM Failed with {error}" )
         sys.exit(1)  # mark server down on error
 
     if output == "1":
+    #if output == "m00tools01":
+        print(f"Success: {output}")
         sys.exit(0)  # healthy
     else:
+        print(f"ext_hm_usr_err_msg: External HM Failed with: {output}")
         sys.exit(1)  # unhealthy
 
-except Exception:
+except Exception as e:
+    print("ext_hm_usr_err_msg: External HM Failed with " + str(e))
     sys.exit(1)  # any exception → unhealthy
